10c750211bf0bbb34706ad3e1b481814e9c2534c
Boris Vujičić <boris.vujicic@boutsourcing.com>
2022-12-12 10:36:14 +0100

    fix ordering of the elements

--- UBL/CAC/TaxSubtotal.php
+++ UBL/CAC/TaxSubtotal.php
@@ -19,9 +19,8 @@ final class TaxSubtotal implements \Stringable
 {
     private const ELEMENT_SIGNATURE = 'cac:TaxSubtotal';
 
-    public readonly TaxCategory $TaxCategory; // [1..1]    The tax category applicable to this subtotal.
-    public readonly TaxAmount $TaxAmount; // [1..1]    The amount of this tax subtotal.
     public readonly ?TaxableAmount $TaxableAmount; // [0..1]    The net amount to which the tax percent (rate) is applied to calculate the tax amount.
+    public readonly TaxAmount $TaxAmount; // [1..1]    The amount of this tax subtotal.
     public readonly ?CalculationSequenceNumeric $CalculationSequenceNumeric; // [0..1]    The number of this tax subtotal in the sequence of subtotals corresponding to the order in which multiple taxes are applied. If all taxes are applied to the same taxable amount (i.e., their order of application is inconsequential), then CalculationSequenceNumeric is 1 for all tax subtotals applied to a given amount.
     public readonly ?TransactionCurrencyTaxAmount $TransactionCurrencyTaxAmount; // [0..1]    The amount of this tax subtotal, expressed in the currency used for invoicing.
     public readonly ?Percent $Percent; // [0..1]    The tax rate of the tax category applied to this tax subtotal, expressed as a percentage.
@@ -29,6 +28,7 @@ final class TaxSubtotal implements \Stringable
     public readonly ?PerUnitAmount $PerUnitAmount; // [0..1]    Where a tax is applied at a certain rate per unit, the rate per unit applied.
     public readonly ?TierRange $TierRange; // [0..1]    Where a tax is tiered, the range of taxable amounts that determines the rate of tax applicable to this tax subtotal.
     public readonly ?TierRatePercent $TierRatePercent; // [0..1]    Where a tax is tiered, the tax rate that applies within a specified range of taxable amounts for this tax subtotal.
+    public readonly TaxCategory $TaxCategory; // [1..1]    The tax category applicable to this subtotal.
 
     public function __construct(TaxCategory $TaxCategory, TaxAmount $TaxAmount, ?TaxableAmount $TaxableAmount, ?CalculationSequenceNumeric $CalculationSequenceNumeric, ?TransactionCurrencyTaxAmount $TransactionCurrencyTaxAmount, ?Percent $Percent, ?BaseUnitMeasure $BaseUnitMeasure, ?PerUnitAmount $PerUnitAmount, ?TierRange $TierRange, ?TierRatePercent $TierRatePercent)
     {
@@ -47,11 +47,10 @@ final class TaxSubtotal implements \Stringable
     public function __toString()
     {
         $response = [];
-        $response[] = (string) $this->TaxCategory;
-        $response[] = (string) $this->TaxAmount;
         if ($this->TaxableAmount) {
             $response[] = (string) $this->TaxableAmount;
         }
+        $response[] = (string) $this->TaxAmount;
         if ($this->CalculationSequenceNumeric) {
             $response[] = (string) $this->CalculationSequenceNumeric;
         }
@@ -73,6 +72,7 @@ final class TaxSubtotal implements \Stringable
         if ($this->TierRatePercent) {
             $response[] = (string) $this->TierRatePercent;
         }
+        $response[] = (string) $this->TaxCategory;
 
         return sprintf(
             '<%s>%s%s%s</%s>',
